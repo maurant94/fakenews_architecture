efe2d24e3cc4811c7bbcd1b203434f90abc76c10
Antonio Mauro <mauro.1766342@studenti.uniroma1.it>
2018-07-03 01:55:26 +0200

    dependencies found

--- queue_layer/src/it/uniroma1/dis/ethereum/SmartContractManager.java
+++ queue_layer/src/it/uniroma1/dis/ethereum/SmartContractManager.java
@@ -1,7 +1,6 @@
 package it.uniroma1.dis.ethereum;
 
 import org.web3j.crypto.Credentials;
-import org.web3j.crypto.WalletUtils;
 import org.web3j.protocol.Web3j;
 import org.web3j.protocol.http.HttpService;
 import org.web3j.tx.Contract;
@@ -9,20 +8,14 @@ import org.web3j.tx.ManagedTransaction;
 
 public class SmartContractManager {
    public static void main(String[] args) throws Exception {
-	   String rinkebyKey = args[0];
-	   String walletFilePassword = args[1];
-	  
-	   String rinkebyUrl = "https://rinkeby.infura.io/" + rinkebyKey;
-	   Web3j web3j = Web3j.build(new HttpService(rinkebyUrl));
-
-	   String walletId = "4a50789f96b4121a14974d607d6a0f5384ec4af4";
-	   String walletSource = "~/testnet-keystore/UTC--2017-10-12T11-10-59.66000000Z--" + walletId + ".json";
-	   Credentials credentials = WalletUtils.loadCredentials(walletFilePassword, walletSource);
+	   
+	   Web3j web3j = Web3j.build(new HttpService("http://localhost:7545"));  // defaults to http://localhost:8545/
+	   Credentials credentials = Credentials.create("5c71e8cfae6e0cb8c80602d2f1fc66d1fca5674dd6f2ff05b4908c0156c777c5");
 
 	   FiveW contract = FiveW.deploy( web3j, credentials, 
 			   ManagedTransaction.GAS_PRICE, Contract.GAS_LIMIT).send();
 
 	   
-	   System.out.println("NOW HERE");
+	   System.out.println(credentials.getAddress());
 }
 }
