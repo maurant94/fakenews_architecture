4ff827352b794667584808b6a47a79593ce9b295
Antonio Mauro <mauro.1766342@studenti.uniroma1.it>
2018-07-13 12:48:06 +0200

    ending writing smart contract for simulating layer2

---
+++ queue_layer/src/it/uniroma1/dis/ethereum/Vote.java
@@ -0,0 +1,45 @@
+package it.uniroma1.dis.ethereum;
+
+import java.math.BigInteger;
+import org.web3j.crypto.Credentials;
+import org.web3j.protocol.Web3j;
+import org.web3j.protocol.core.RemoteCall;
+import org.web3j.tx.Contract;
+import org.web3j.tx.TransactionManager;
+
+/**
+ * <p>Auto generated code.
+ * <p><strong>Do not modify!</strong>
+ * <p>Please use the <a href="https://docs.web3j.io/command_line.html">web3j command line tools</a>,
+ * or the org.web3j.codegen.SolidityFunctionWrapperGenerator in the 
+ * <a href="https://github.com/web3j/web3j/tree/master/codegen">codegen module</a> to update.
+ *
+ * <p>Generated with web3j version 3.4.0.
+ */
+public class Vote extends Contract {
+    private static final String BINARY = "6080604052348015600f57600080fd5b5060008054600160a060020a03191633179055603580602f6000396000f3006080604052600080fd00a165627a7a72305820ed50fa32979cd45d4c932e2d80db31f4fd777468c99949efde553edb3932de9f0029";
+
+    protected Vote(String contractAddress, Web3j web3j, Credentials credentials, BigInteger gasPrice, BigInteger gasLimit) {
+        super(BINARY, contractAddress, web3j, credentials, gasPrice, gasLimit);
+    }
+
+    protected Vote(String contractAddress, Web3j web3j, TransactionManager transactionManager, BigInteger gasPrice, BigInteger gasLimit) {
+        super(BINARY, contractAddress, web3j, transactionManager, gasPrice, gasLimit);
+    }
+
+    public static RemoteCall<Vote> deploy(Web3j web3j, Credentials credentials, BigInteger gasPrice, BigInteger gasLimit) {
+        return deployRemoteCall(Vote.class, web3j, credentials, gasPrice, gasLimit, BINARY, "");
+    }
+
+    public static RemoteCall<Vote> deploy(Web3j web3j, TransactionManager transactionManager, BigInteger gasPrice, BigInteger gasLimit) {
+        return deployRemoteCall(Vote.class, web3j, transactionManager, gasPrice, gasLimit, BINARY, "");
+    }
+
+    public static Vote load(String contractAddress, Web3j web3j, Credentials credentials, BigInteger gasPrice, BigInteger gasLimit) {
+        return new Vote(contractAddress, web3j, credentials, gasPrice, gasLimit);
+    }
+
+    public static Vote load(String contractAddress, Web3j web3j, TransactionManager transactionManager, BigInteger gasPrice, BigInteger gasLimit) {
+        return new Vote(contractAddress, web3j, transactionManager, gasPrice, gasLimit);
+    }
+}
